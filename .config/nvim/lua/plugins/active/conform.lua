fb2804ca45460c1bb9be132e94764df4776d88ac
= <=>
2025-08-20 21:57:09 +0100

    Added conform to nvim config

---
+++ .config/nvim/lua/plugins/active/conform.lua
@@ -0,0 +1,18 @@
+return {
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "isort", "black" },
+			javascript = { "prettierd", "prettier", stop_after_first = true },
+			typescript = { "prettierd", "prettier", stop_after_first = true },
+			fish = { "fish_indent" },
+			sql = { "sqlfmt" },
+			yaml = { "yamlfmt" },
+		},
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_format = "fallback",
+		},
+	},
+}
